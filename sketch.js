71ecd7320f1a2d257f8da2020eeec9f871af8dd3
Kevin Baker <kbaker@wlapcs.org>
2017-04-11 17:24:29 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+    createCanvas(600,400);
+    background(0);
+}
+
+function draw(){
+    strokeWeight(4);
+    stroke(255);
+    
+     var x = 0;
+    while(x<650){
+        
+    }
+    
+if(mouseIsPressed);
+ 
+  
+  
+   }
+
+
